e3b848847737c90788ea6f5e7b4775f92cb49688
lafest <lafest@minding.today>
2021-07-29 20:31:28 +0900

    add: install material-ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,32 @@
+import { Button } from "@material-ui/core";
 import { useEffect, useState } from "react";
+
+const dateArr = [
+  '일요일',
+  '월요일',
+  '화요일',
+  '수요일',
+  '목요일',
+  '금요일',
+  '토요일',
+];
 
 export const App = () => {
   const [counter, setCounter] = useState(10);
 
   useEffect(() => {
-    setInterval(() => setCounter((c) => c - 0.01), 10)
+    const timer = setInterval(() => setCounter((c) => c - 0.01), 10);
+    return () => clearInterval(timer);
   }, [])
 
   return (
-    <div className="App">
+    <div>
       {counter}
+      <div>
+        {dateArr.map((date) => (
+          <Button variant='contained'>{date}</Button>        
+        ))}
+      </div>
     </div>
   );
 }
