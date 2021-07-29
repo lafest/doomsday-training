982ae99a20de0dbc82ab0ae30203458c4d626a3f
lafest <lafest@minding.today>
2021-07-29 21:41:44 +0900

    add: install @types/material-ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,24 @@
-import { Button } from "@material-ui/core";
-import { useEffect, useState } from "react";
-
-const dateArr = [
-  '일요일',
-  '월요일',
-  '화요일',
-  '수요일',
-  '목요일',
-  '금요일',
-  '토요일',
-];
+import { Route, BrowserRouter, Switch, Redirect } from 'react-router-dom';
+import { GamePage } from './components/GamePage';
+import { StartPage } from './components/StartPage';
 
 export const App = () => {
-  const [counter, setCounter] = useState(10);
-
-  useEffect(() => {
-    const timer = setInterval(() => setCounter((c) => c - 0.01), 10);
-    return () => clearInterval(timer);
-  }, [])
-
   return (
-    <div>
-      {counter}
-      <div>
-        {dateArr.map((date) => (
-          <Button variant='contained'>{date}</Button>        
-        ))}
-      </div>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <Redirect to="/start" />
+        </Route>
+        <Route path="/start">
+          <StartPage />
+        </Route>
+        <Route path="/play">
+          <GamePage />
+        </Route>
+        <Route path="/gameover">
+          <StartPage />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
